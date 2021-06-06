69419044751f988b9f31ae1cd704fe7ea38a03a9
Juan Alejandro Corchado Almeida <56848879+alej404@users.noreply.github.com>
2021-06-06 12:04:58 -0300

    primeiroJogo

---
+++ AC14112/Assets/Scripts/Personagem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Personagem : MonoBehaviour
+{
+    Rigidbody2D personagem;
+    int velocidade;
+    int pulo;
+    bool pulando;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        personagem = GetComponent<Rigidbody2D>();
+        pulando = true;
+        velocidade = 3;
+        pulo = 12;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mover();
+        Pular();
+    }
+
+    void Mover()
+    {
+        if(Input.GetKey("right"))
+        {
+            personagem.velocity = new Vector2(velocidade, personagem.velocity.y);
+        }
+        else if(Input.GetKey("left"))
+        {
+            personagem.velocity = new Vector2(-velocidade, personagem.velocity.y);
+        }
+    }
+
+    void Pular()
+    {
+        if(Input.GetKey("up") && !pulando)
+        {
+            personagem.velocity = new Vector2(personagem.velocity.x, pulo);
+            pulando = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D pisando)
+    {
+        if(pisando.gameObject.tag == "Plataforma")
+        {
+            pulando = false;
+        }
+        if(pisando.gameObject.tag == "Armadilha")
+        {
+            SceneManager.LoadScene("Jogo");
+        }
+    }
+}
---
+++ tentativaMudarDirecaoPersonagem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private Rigidbody2D rb2D;
+    private bool eLadoDireito;
+
+    // Variavel
+    float horizontal; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        eLadoDireito = transform.localScale.x > 0;        
+    } 
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        MudarDirecao(horizontal);
+    }
+
+
+
+    private void MudarDirecao(float horizontal)
+    {
+        if(horizontal > 0 && !eLadoDireito || horizontal < 0 && eLadoDireito)
+        {
+            eLadoDireito = !eLadoDireito;
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
